25dcdd3a263fdfdbc4367c4d699f0443069d034e
Evan Voeltner <evanvoeltner@gmail.com>
2022-09-06 11:39:14 -0500

    feat: created a less than one hundred and repeated names function

--- algorithms.py
+++ algorithms.py
@@ -1,6 +1,6 @@
 def even_or_odd(given_int):
     """
-    The given code will always run once making it a constant
+    The given code will always run once making it a constant O(1)
     No matter the size of the number, the amount of steps remain the same
     """
     if given_int % 2 == 0:
@@ -10,3 +10,32 @@ def even_or_odd(given_int):
 
 print(even_or_odd(2))
 print(even_or_odd(5))
+
+def less_than_one_hundred(given_list):
+    """
+    The given code will run more depending on the size of the input making it linear O(n)
+    Depending on the size of the list, the for loop will iterate more times
+    """
+    for num in given_list:
+        if num > 100:
+            return False
+    return True
+
+print(less_than_one_hundred([2,53,15,84,84,17]))
+print(less_than_one_hundred([2,53,15,84,84,1327]))
+
+def repeated_names(list_of_names):
+    """
+     The given code will run more depending on the size of the input making it Quadratic O(n^2)
+     The bigger the list_of_names get, the more it will have to check a single element to EVERY OTHER ELEMENT
+    """
+    for i in range(len(list_of_names)):
+        for j in range(len(list_of_names)):
+            if i == j:
+                break
+            if list_of_names[i] == list_of_names[j]:
+                return True
+    return False
+
+print(repeated_names(['jamie','lanny','evan','jj','nevin','evan']))
+print(repeated_names(['jamie','lanny','evan','jj','nevin']))
